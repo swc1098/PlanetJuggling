ecf8911770fcc0cc8cb095efc23dbe8ce4231fe9
Zach Bebel <zlb3507@rit.edu>
2016-04-15 13:50:41 -0400

    menus working except start button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,97 +1,212 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour {
+public enum GameState
+{
+	Start,
+	Game,
+	Pause,
+	Win,
+	Lose
+}
 
-    // UI text to keep track of
-    public Text scoreKeep;
-    public Text ballBounce;
-    public Text gameOver;
-    public Text information;
-    public Text leaveText;
+public class GameManager : MonoBehaviour
+{
 
-    // camera settings
-    public float maxCameraZoom;
-    public float minCameraZoom;
+	// UI text to keep track of
+	public Text scoreKeep;
+	public Text ballBounce;
+	public Text gameOver;
+	public Text information;
+	public Text leaveText;
 
-    // a check to see if game needs to be zoomed
-    public bool zoomed = false;
+	// camera settings
+	public float maxCameraZoom;
+	public float minCameraZoom;
 
-    // check to see if game is in a start state
-    public bool start = true;
+	// a check to see if game needs to be zoomed
+	public bool zoomed = false;
 
-    // int to be parsed into strings
-    public int score;
-    public int bounceCount;
+	// keep track of game state
+	public GameState gameState;
+	private GameState currentState;
+	private GameState previousState;
 
-    // Use this for initialization
-    void Start ()
-    {
-        // initial settings
-        minCameraZoom = 8;
-        maxCameraZoom = 12.5f;
-        score = 0;
-        bounceCount = 0;
+	// menus & buttons
+	private Dictionary<GameState, GameObject> menus;
+	private GameObject[] startButton;
+	private GameObject[] menuButton;
+	private GameObject[] resumeButton;
+	private GameObject[] restartButton;
 
-        // keep the main camera as always orthographic
-        Camera.main.orthographic = true;
+	// int to be parsed into strings
+	public int score;
+	public int bounceCount;
 
-    }
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		menus = new Dictionary<GameState, GameObject> ();
+		menus.Add (GameState.Start, GameObject.Find ("MainMenu"));
+		menus.Add (GameState.Game, GameObject.Find ("GameMenu")); // placeholder
+		menus.Add (GameState.Pause, GameObject.Find ("PauseMenu"));
+		menus.Add (GameState.Win, GameObject.Find ("VictoryMenu"));
+		menus.Add (GameState.Lose, GameObject.Find ("GameOverMenu"));
+		foreach (GameObject m in menus.Values) {
+			m.GetComponent<RectTransform>().localPosition = Vector3.zero;
+		}
+
+		// Start --> Game
+		startButton = GameObject.FindGameObjectsWithTag ("StartButton");
+		foreach (GameObject button in startButton) {
+			button.GetComponent<Button>().onClick.AddListener(() => { // anonymous (delegate) function!
+				ChangeState (GameState.Game);
+				NewGame ();
+			});
+		}
+
+		// ___ --> Start
+		menuButton = GameObject.FindGameObjectsWithTag ("MenuButton");
+		foreach (GameObject button in menuButton) {
+			button.GetComponent<Button>().onClick.AddListener(() => { // anonymous (delegate) function!
+				ChangeState (GameState.Start);
+			});
+		}
+
+		// Pause --> Game
+		resumeButton = GameObject.FindGameObjectsWithTag ("ResumeButton");
+		foreach (GameObject button in resumeButton) {
+			button.GetComponent<Button>().onClick.AddListener(() => { // anonymous (delegate) function!
+				ChangeState (GameState.Game);
+			});
+		}
+
+		// ___ --> Game
+		restartButton = GameObject.FindGameObjectsWithTag ("RestartButton");
+		foreach (GameObject button in restartButton) {
+			button.GetComponent<Button>().onClick.AddListener(() => { // anonymous (delegate) function!
+				ChangeState (GameState.Game);
+				NewGame ();
+			});
+		}
+
+		// keep the main camera as always orthographic
+		Camera.main.orthographic = true;
+		minCameraZoom = 8;
+		maxCameraZoom = 12.5f;
+
+		// Set active menu to start game
+		ChangeState (GameState.Game);
+	}
+
+	void NewGame ()
+	{
+		// initial settings
+		score = 0;
+		bounceCount = 0;
+	}
 	
 	// Update is called once per frame
 	void Update ()
-    {
-        // update scores
-        ballBounce.text = bounceCount.ToString();
-        scoreKeep.text = score.ToString();
+	{
+		currentState = gameState;
+		if (currentState != previousState) {
+			/*
+			switch (currentState) {
+			case GameState.Start:
+				Debug.Log ("Start");
+				break;
+			case GameState.Game:
+				Debug.Log ("Game");
+				break;
+			case GameState.Pause:
+				Debug.Log ("Pause");
+				break;
+			case GameState.Win:
+				Debug.Log ("Win");
+				break;
+			case GameState.Lose:
+				Debug.Log ("Lose");
+				break;
+			}*/
+		}
 
-        // just a check to see whether or not to zoom. Calls methods accordingly.
-        if (zoomed == true)
-        {
-            zoomOut();
-        }
+		if (gameState == GameState.Game) {
 
-        if (zoomed == false && start == false)
-        {
-            zoomIn();
-        }
-    }
+			// update scores
+			ballBounce.text = bounceCount.ToString ();
+			scoreKeep.text = score.ToString ();
+
+			// just a check to see whether or not to zoom. Calls methods accordingly.
+			if (zoomed == true) {
+				zoomOut ();
+			} else {
+				zoomIn ();
+			}
+
+		}
+
+		// Check for pause state
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (gameState == GameState.Game) {
+				ChangeState(GameState.Pause);
+			} else if (gameState == GameState.Pause) {
+				ChangeState(GameState.Game);
+			}
+		}
+
+		previousState = currentState;
+	}
+		
+	//
+	//
+	// CHANGE GAMESTATE HERE
+	//
+	//
+	public void ChangeState(GameState state) {
+		gameState = state;
+
+		foreach (GameObject m in menus.Values) {
+			m.SetActive(false);
+		}
+
+		menus[state].SetActive(true);
+	}
 
 
-    // handles zoom sloppily. It's a placeholder until we refine it.
-    public void OnTriggerExit(Collider c)
-    {
-        if(c.tag == "Ball" && zoomed == false)
-        {
-            zoomed = true;
-            start = false;
-        }
-    }
+	// handles zoom sloppily. It's a placeholder until we refine it.
+	public void OnTriggerExit (Collider c)
+	{
+		if (c.tag == "Ball" && zoomed == false) {
+			zoomed = true;
+			gameState = GameState.Game;
+		}
+	}
 
-    public void OnTriggerEnter(Collider c)
-    {
-        if (c.tag == "Ball" && zoomed == true)
-        {
-            zoomed = false;
-        }
-    }
-
-    // Figure out the camera's current position and then lerp to desired zoomsize
-    public void zoomOut()
-    {
-        for (float i = minCameraZoom; i < maxCameraZoom; i++)
-        {
-            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, maxCameraZoom, Time.deltaTime);
-        }
-    }
+	public void OnTriggerEnter (Collider c)
+	{
+		if (c.tag == "Ball" && zoomed == true) {
+			zoomed = false;
+		}
+	}
 
 	// Figure out the camera's current position and then lerp to desired zoomsize
-    public void zoomIn()
-    {
-        for (float i = maxCameraZoom; i > minCameraZoom; i--)
-        {
-            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, minCameraZoom, Time.deltaTime);
-        }
-    }
+	public void zoomOut ()
+	{
+		for (float i = minCameraZoom; i < maxCameraZoom; i++) {
+			Camera.main.orthographicSize = Mathf.Lerp (Camera.main.orthographicSize, maxCameraZoom, Time.deltaTime);
+		}
+	}
+
+	// Figure out the camera's current position and then lerp to desired zoomsize
+	public void zoomIn ()
+	{
+		for (float i = maxCameraZoom; i > minCameraZoom; i--) {
+			Camera.main.orthographicSize = Mathf.Lerp (Camera.main.orthographicSize, minCameraZoom, Time.deltaTime);
+		}
+	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,29 +29,29 @@ public class Player : MonoBehaviour
 		// player spin animation
 		transform.Rotate(Vector3.up, 30f * Time.deltaTime);
 
-		// basic movement around planet
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.RotateAround(GameObject.Find("MainPlanet").transform.position, new Vector3(0, 0, 1.0f), speed * Time.deltaTime);
-			transform.Rotate(Vector3.left, 800f * Time.deltaTime);
-        }
+		if (gm.gameState == GameState.Game) {
+			
+			// basic movement around planet
+			if (Input.GetKey (KeyCode.A)) {
+				transform.RotateAround (GameObject.Find ("MainPlanet").transform.position, new Vector3 (0, 0, 1.0f), speed * Time.deltaTime);
+				transform.Rotate (Vector3.left, 800f * Time.deltaTime);
+			}
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.RotateAround(GameObject.Find("MainPlanet").transform.position, new Vector3(0, 0, -1.0f), speed * Time.deltaTime);
-			transform.Rotate(Vector3.right, 800f * Time.deltaTime);
-        }
+			if (Input.GetKey (KeyCode.D)) {
+				transform.RotateAround (GameObject.Find ("MainPlanet").transform.position, new Vector3 (0, 0, -1.0f), speed * Time.deltaTime);
+				transform.Rotate (Vector3.right, 800f * Time.deltaTime);
+			}
 
-        // add force to player's jump
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            //rb.AddRelativeForce(Vector3.up * 120);
-            //falling = true;
-            // spawns object on top of player. 
-            ball = (GameObject)Instantiate(Resources.Load("Ball"), gameObject.transform.position * 2, Quaternion.identity) as GameObject;
-			ball.transform.SetParent(GameObject.Find("SolarSystem").transform);
-            gm.information.enabled = false;
-        }
+			// add force to player's jump
+			if (Input.GetKeyDown (KeyCode.W)) {
+				//rb.AddRelativeForce(Vector3.up * 120);
+				//falling = true;
+				// spawns object on top of player. 
+				ball = (GameObject)Instantiate (Resources.Load ("Ball"), gameObject.transform.position * 2, Quaternion.identity) as GameObject;
+				ball.transform.SetParent (GameObject.Find ("SolarSystem").transform);
+				gm.information.enabled = false;
+			}
+		}
     }
 
     // make sure that the player's falling bool is not affected by touching collectibles
